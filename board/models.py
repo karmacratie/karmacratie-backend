ec34e58053bd88df6b67a0f548c5447583cfb99d
Karmacrate <source-master@karmacratie.com>
2018-02-06 16:01:50 +0100

    fix models definition

--- board/models.py
+++ board/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Ministry(models.Model):
     name = models.CharField("Nom du ministere", max_length=128, blank=False, null=False, unique=True)
-    description = models.CharField("Description du ministère", max_length=1024, blank=False, null=False)
+    description = models.TextField("Description du ministère", blank=False, null=False)
     admin = models.ForeignKey(User)
 
 class Cycle(models.Model):
@@ -15,17 +15,16 @@ class Cycle(models.Model):
     start_date = models.DateTimeField(auto_now_add=True)
     end_date = models.DateTimeField(blank=False, null=False)
     active = models.BooleanField(default=True)
-    next_cycle = models.ForeignKey(Cycle, null=True, blank=True)
+    next_cycle = models.ForeignKey("Cycle", null=True, blank=True)
 
 class Post(models.Model):
     owner = models.ForeignKey(User)
     creation_date = models.DateTimeField(auto_now_add=True)
-    parent_post = models.ForeignKey(Post)
+    parent_post = models.ForeignKey("Post")
     parent_ministry = models.ForeignKey(Ministry)
-    title = models.CharField("Titre de votre contribution", blank=False, null=False)
-    content = models.CharField("Contenu de votre contribution", blank=False, null=False)
-    votes = models.ManyToManyField(Vote)
-    cycle = models.ForeignKey(Cycle)
+    title = models.CharField("Titre de votre contribution", max_length=256, blank=False, null=False)
+    content = models.TextField("Contenu de votre contribution", blank=False, null=False)
+    cycle = models.ForeignKey("Cycle")
     
 
 class Karma(models.Model):
