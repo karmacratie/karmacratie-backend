8772ab9ce51eb067628887f299041f83cdc94656
Gérald Colangelo <gcolangelo@yanluo.net>
2018-02-08 07:53:18 +0100

    first working example of REST API

--- board/views.py
+++ board/views.py
@@ -5,9 +5,9 @@ from board.serializers import MinistrySerializer, CycleSerializer
 # Create your views here.
 class MinistryViewSet(viewsets.ModelViewSet):
     queryset = Ministry.objects.all().order_by('name')
-    serializer_class MinistrySerializer
+    serializer_class = MinistrySerializer
 
 
 class CycleViewSet(viewsets.ModelViewSet):
     queryset = Cycle.objects.all().order_by('name')
-    serializer_class CycleSerializer
+    serializer_class = CycleSerializer
