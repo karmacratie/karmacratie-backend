ed5190516265344a751e1fdf2fe50299279c9960
Karmacratie <source-master@karmacratie.com>
2018-02-08 07:42:15 +0100

    Experimenting rest-framework for python KO: probably version mismatch

--- board/views.py
+++ board/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from board.models import Ministry, Cycle
+from board.serializers import MinistrySerializer, CycleSerializer
 
 # Create your views here.
+class MinistryViewSet(viewsets.ModelViewSet):
+    queryset = Ministry.objects.all().order_by('name')
+    serializer_class MinistrySerializer
+
+
+class CycleViewSet(viewsets.ModelViewSet):
+    queryset = Cycle.objects.all().order_by('name')
+    serializer_class CycleSerializer
--- karmacratie/urls.py
+++ karmacratie/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from rest_framework import routers
+from rest_framework import compat
+from board import views 
+
+router = routers.DefaultRouter()
+router.register(r'ministry', views.MinistryViewSet)
+router.register(r'cycle', views.CycleViewSet)
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'karmacratie.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^', include(router.urls)),
     url(r'^admin/', include(admin.site.urls)),
 )
