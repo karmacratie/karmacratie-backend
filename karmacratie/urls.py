3c5245f6b438acbac5177459bf063650e91b91ce
Karmacratie <source-master@karmacratie.com>
2018-12-07 12:51:54 +0100

    Setup TEMPLATE var to make it run with django 1.11 Remove references to patter() (outdated)

--- karmacratie/urls.py
+++ karmacratie/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 from rest_framework import routers
 from rest_framework import compat
@@ -8,7 +8,7 @@ router = routers.DefaultRouter()
 router.register(r'ministry', views.MinistryViewSet)
 router.register(r'cycle', views.CycleViewSet)
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^admin/', include(admin.site.urls)),
-)
+]
